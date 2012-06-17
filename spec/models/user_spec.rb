5ab6c1c50876b9d80f4c1eaa351cc02a450b3185
Joe Hall <Joseph.L.Hall@gmail.com>
2012-06-16 22:39:01 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -93,8 +94,14 @@ describe User do
 
       describe "with invalid password" do
         let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+        
         it { should_not == user_for_invalid_password }
         specify { user_for_invalid_password.should be_false }
       end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,7 @@ describe "Static pages" do
 
       it { should have_selector('h1', text: 'Sample App') }
       it {should have_selector('title', text: full_title('')) }
-      it {should_not have_selector('title', text: '| Home') } 
+      it {should_not have_selector 'title', text: '| Home' } 
     end
 
   describe "Help page" do
@@ -36,6 +36,8 @@ end
 
   it "should have the right links to the layout" do
     visit root_path
+    click_link "Sign in"
+    page.should have_selector 'title', text: full_title('Sign in')
     click_link "About"
     page.should have_selector 'title', text: full_title('About Us')
     click_link "Help"
